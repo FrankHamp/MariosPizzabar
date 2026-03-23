4f0267275acef5728d2dae8f87e49b626cd97097
Jeppeoberg <jeob1000@stud.ek.dk>
2026-03-23 13:34:42 +0100

    endelig måske udelukkende pizzaui klassen med det hele

--- src/ui/PizzaBarUI.java
+++ src/ui/PizzaBarUI.java
@@ -33,138 +33,103 @@ printer hele pizza-menuen
 
 public class PizzaBarUI {
 
-package ui;
-import model.*;
-import file.*;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import service.*;
-
-import java.util.ArrayList;
-
-/* + void start()
-- Greeting
-+ void ShowInterface
-+ void addOrder()
-printer hele pizza-menuen
-
---------------------------------
-+ void removeOrder()
-+ void showOrder()
-+ void clearOrder()
---------------------------------
-
-- ArrayList<Pizza> menuItems
-- ArrayList<Order> activeOrders
-- Scanner scanner
-- OrderFileHandler fileHandler
-
-+ void exitProgram() */
-
-    public class PizzaBarUI {
-
-        private ArrayList<Pizza> menuItems = new ArrayList<>();
-        //læser items fra marius' tekstfil og smider hver linje ind i et arraylist
-        private ArrayList<Order[]> activeOrders = new ArrayList<>();
-        private Scanner scanner;
-        private OrderHandler fileHandler;
+    private ArrayList<Pizza> menuItems = new ArrayList<>();
+    //læser items fra marius' tekstfil og smider hver linje ind i et arraylist
+    private ArrayList<Order[]> activeOrders = new ArrayList<>();
+    private Scanner scanner;
+    private OrderHandler fileHandler;
 
 
 
-        public PizzaBarUI() {
-            scanner = new Scanner(System.in);
-            fileHandler = new OrderHandler();
-        }
+    public PizzaBarUI() {
+        scanner = new Scanner(System.in);
+        fileHandler = new OrderHandler();
+    }
 
-        public void start() {
+    public void start() {
 
-            boolean running = true;
+        boolean running = true;
 
-            while (running) {
+        while (running) {
 
-                showInterface();
-
-                try {
-
-                    int choice = scanner.nextInt();
-
-                    switch (choice) {
-
-                        case 1:
-                            addOrder();
-                            break;
-
-                        case 2:
-                            removeOrder();
-                            break;
-
-                        case 3:
-                            showOrder();
-                            break;
-
-                        case 4:
-                            clearOrder();
-                            break;
-
-                        case 5:
-                            exitProgram();
-                            break;
-
-                    }
-
-                }
-                catch() {
-                    //lav errorhandler senere
-                }
-            }
-        }
-
-        public void showInterface() {
-
-            System.out.println("\nVelkommen Alfonzo!");
-            System.out.println("1. Tilføj ordre");
-            System.out.println("2. Fjern ordre(r)");
-            System.out.println("3. vis ordre(r)");
-            System.out.println("4. ryd ordrer");
-            System.out.println("5. Afbryd");
-        }
-
-        private void showMenu() {
-
-            //skal muligvis laves om -------------------
-            String filePath = "menukort.csv";
-            String line;
-
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(filePath));
-
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-
-                // midlertidig Error ---------------------
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-
-        private void addOrder() {
+            showInterface();
 
             try {
 
-                showMenu();
+                int choice = scanner.nextInt();
 
-                System.out.print("Hvor mange pizzaer vil du købe?");
-                int numberOfPizzas = scanner.nextInt();
-                scanner.nextLine();
+                switch (choice) {
+
+                    case 1:
+                        addOrder();
+                        break;
+
+                    case 2:
+                        removeOrder();
+                        break;
+
+                    case 3:
+                        showOrder();
+                        break;
+
+                    case 4:
+                        clearOrder();
+                        break;
+
+                    case 5:
+                        exitProgram();
+                        break;
+
+                }
+
+            }
+            catch() {
+                //lav errorhandler senere
+            }
+        }
+    }
+
+    public void showInterface() {
+
+        System.out.println("\nVelkommen Alfonzo!");
+        System.out.println("1. Tilføj ordre");
+        System.out.println("2. Fjern ordre(r)");
+        System.out.println("3. vis ordre(r)");
+        System.out.println("4. ryd ordrer");
+        System.out.println("5. Afbryd");
+    }
+
+    private void showMenu() {
+
+        //skal muligvis laves om -------------------
+        String filePath = "menukort.csv";
+        String line;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // midlertidig Error ---------------------
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void addOrder() {
+
+        try {
+
+            showMenu();
+
+            System.out.print("Hvor mange pizzaer vil du købe?");
+            int numberOfPizzas = scanner.nextInt();
+            scanner.nextLine();
 
 
 
@@ -178,33 +143,28 @@ printer hele pizza-menuen
             String name = scanner.nextLine();
             cats[i] = new Cat(name); */
 
-                activeOrders.add(new Order[0]);
+            activeOrders.add(new Order[0]);
 
-            } catch (Exception e) { //----------------------
-                throw new RuntimeException(e);
-            }
-
+        } catch (Exception e) { //----------------------
+            throw new RuntimeException(e);
         }
 
-        private void removeOrder() {
+    }
+
+    private void removeOrder() {
 
 
-        }
+    }
 
-        private void showOrder() {
+    private void showOrder() {
 
-        }
+    }
 
+    private void clearOrder() {
 
-        private void clearOrder() {
+    }
 
-        }
-
-        private void exitProgram() {
-
-        }
-
-
+    private void exitProgram() {
 
     }
 
