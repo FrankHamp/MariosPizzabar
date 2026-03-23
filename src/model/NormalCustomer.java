4dce6b769b06cc4969e98af14f8491b1286598d6
MariusRask <mara1010@stud.ek.dk>
2026-03-23 12:43:34 +0100

    Customer class

--- src/model/NormalCustomer.java
+++ src/model/NormalCustomer.java
@@ -1,4 +1,21 @@
 package model;
 
 public class NormalCustomer extends Customer {
+
+    public NormalCustomer(String customerName) {
+        super(customerName); // Kalder på konstruktøren af Customer
+    }
+
+    /*
+     Overrider udregningen af pris i Order
+     I super klassen bliver total prisen * 1
+     her er det 1 - 0.0. = 1, hvilket vil sige, at prisen bliver ganget med 1
+     derved er der ikke blevet trukket rabat
+     */
+
+@Override
+    public double getDiscountRate(){
+        return 0.0;
+}
+
 }
